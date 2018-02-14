c8e0d0c22b9c09ef326b0eed624474dcadeb5835
Eddie Cornelious <eulerissmart@gmail.com>
2018-02-14 16:43:15 +0000

    implemented set interface

---
+++ src/Set.js
@@ -0,0 +1,28 @@
+
+import SetInterface from './SetInterface';
+import RBTree from './RedBlackTree.js';
+
+class Set extends SetInterface {
+  constructor(comparator) {
+    super();
+    this.set = new RBTree(comparator);
+  }
+  
+  add(element) {
+    return this.set.insert(element);
+  }
+  
+  has(element) {
+    return this.set.contains(element);
+  }
+  
+  remove(element) {
+    return this.set.remove(element);
+  }
+  
+  cardinality() {
+    return 0;
+  }
+}
+
+module.exports = Set;
