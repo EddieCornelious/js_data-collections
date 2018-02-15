4ce1c26b14e013a63d51eac9f0ef5528c4e3ef99
Eddie Cornelious <eulerissmart@gmail.com>
2018-02-15 19:00:13 +0000

    minor change

--- src/Map.js
+++ src/Map.js
@@ -2,33 +2,41 @@
 import MapInterface from "./MapInterface.js";
 import RBTree from "./RedBlackTree.js";
 
+/**
+ * Map representaion
+ * @class
+ * @implements MapInterface
+ * @param {function} comparator - @see Global#defaultComparator
+ */
 class Map extends MapInterface {
-  constructor() {
-    this.map = new RBTree();  
+  constructor(comparator) {
+    super();
+    this.map = new RBTree(comparator);  
   }
-  
+
   put(key, value) {
     return this.map.insert(key, value);
   }
-  
+
   getVal(key) {
     return this.map.find(key);
   }
-  
+
   remove(key) {
     return this.map.remove(key);
   }
-  
+
   keys() {
     return this.map.keys();
   }
-  
+
   contains(key) {
     return this.map.contains(key);
   }
-  
+
   size() {
     return this.map.size();
   }
- 
 }
+
+module.exports = Map;
