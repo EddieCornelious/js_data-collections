6b0fa15f3a3a1b2ca3b4fa22a1c19fe04e544945
Eddie Cornelious <eulerissmart@gmail.com>
2018-02-22 19:13:54 +0000

    fixed docs and added clear and shortened methods

--- src/Queue.js
+++ src/Queue.js
@@ -15,7 +15,7 @@ class Queue {
 
   /**
    * Inserts given data into queue
-   * @param {*} data - Data to insert into queue
+   * @param {*} data - The data to insert into queue
    * @returns {Queue} The instance that this method was called
    *
    * @example
@@ -23,17 +23,16 @@ class Queue {
    *
    */
   enqueue(data) {
-    const { queue } = this;
-    queue.addToBack(data);
+    this.queue.addToBack(data);
     return this;
   }
 
   /**
-   * Removes from queue in a First in first out manner
+   * Removes from the queue in a first in first out manner
    * @returns {*} The removed data
    * @example
-   * // FROM example above
-   * queue.dequeue() // 1 as it was inserted first
+   * queue.enqueue(1).enqueue(2);
+   * queue.dequeue() // 1
    */
   dequeue() {
     return this.queue.removeFront();
@@ -48,8 +47,15 @@ class Queue {
    * queue.peek() // returns 1 but does not remove it
    */
   peek() {
-    const { queue } = this;
-    return queue.elementAtIndex(0);
+    return this.queue.elementAtIndex(0);
+  }
+
+  /**
+   * Empties the Queue
+   * @returns {undefined}
+   */
+  clear() {
+    return this.queue.clear();
   }
 
   /**
