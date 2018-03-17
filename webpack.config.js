19de4eb76c70d5c687ca4226b8064cb4e36c6e81
Eddie Cornelious <eulerissmart@gmail.com>
2018-03-17 04:14:28 +0000

    removed polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,6 @@
-if(!Object.create){
-Object.create = function(proto, propertiesObject) {
-  if (typeof proto !== "object" && typeof proto !== "function") {
-    throw new TypeError("Object prototype may only be an Object: " + proto);
-  } else if (proto === null) {
-    throw new Error(
-      "This browser's implementation of Object.create is a shim and doesn't support 'null' as the first argument."
-    );
-  }
-
-  if (typeof propertiesObject != "undefined") {
-    throw new Error(
-      "This browser's implementation of Object.create is a shim and doesn't support a second argument."
-    );
-  }
-
-  function F() {}
-  F.prototype = proto;
-
-  return new F();
-};
-}
 
 module.exports = {
-  entry: './src/main.js',
+  entry: ['./poly.js','./src/main.js'],
   output: {
     path: __dirname,
     filename: 'collections.js',
@@ -47,7 +25,6 @@ module.exports = {
         loader: 'eslint-loader',
         exclude: /node_modules/,
         watch: true
-        
       }
     ]
   }
