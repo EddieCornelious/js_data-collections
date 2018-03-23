c8bf8c4f08e8590ad1c2684eb6df3ed9b5670eff
Eddie Cornelious <eulerissmart@gmail.com>
2018-03-23 00:20:51 +0000

    removed ie8 attempt support

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,7 @@
 
 module.exports = {
   entry: [
-  "core-js/fn/array/map",
-  "core-js/fn/array/filter",
-  "core-js/fn/array/for-each",
-  "core-js/fn/array/index-of",
   "core-js/fn/object/define-property",
-  "core-js/fn/object/keys",
-  "core-js/fn/object/get-own-property-descriptor",
-  "core-js/fn/object/get-own-property-names", 
-  "core-js/fn/object/create",
   './src/main.js'],
   output: {
     path: __dirname,
@@ -27,8 +19,7 @@ module.exports = {
         query: {
           presets: [ ['es2015', {"loose": true}]],  
           plugins: [ ["transform-es3-member-expression-literals"], ["transform-es3-property-literals"], ["transform-proto-to-assign"], 
-          ["transform-es2015-classes", {"loose": true}], ["transform-object-set-prototype-of-to-assign"],
-          ["transform-jscript"]]
+          ["transform-es2015-classes", {"loose": true}], ["transform-object-set-prototype-of-to-assign"]]
         }, 
   
       }, 
