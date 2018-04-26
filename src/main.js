22540256acf68bd77bc963b023ddc64dd42b8338
Eddie Cornelious <eulerissmart@gmail.com>
2018-04-26 13:24:52 +0000

    removed red blcak tree and hashtable from public api

--- src/main.js
+++ src/main.js
@@ -5,14 +5,12 @@ import Queue from './Queue.js';
 import BHeap from './BHeap.js';
 import PriorityQueue from './PriorityQueue.js';
 import HashMap from './HashMap.js';
-import HashTable from './HashTable.js';
 import HashSet from './HashSet.js';
 import  BST from './BST.js';
 import Graph from './Graph.js';
 import Trie from './Trie.js';
 import HashMultiMap from './HashMultiMap.js';
 import ArrayUtils from './ArrayUtils.js';
-import RBTree from  './RedBlackTree.js';
 import Map from './Map.js';
 import Set from './Set.js';
 
@@ -23,14 +21,12 @@ export {
    BHeap,
    PriorityQueue,
    HashMap,
-   HashTable,
    HashMultiMap,
    HashSet,
    BST,
    Graph,
    Trie,
    ArrayUtils,
-   RBTree,
    Map,
    Set
 };
