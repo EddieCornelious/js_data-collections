c5f87bb1c3d460b2fef020532a4d7cbada0453a9
Eddie Cornelious <eulerissmart@gmail.com>
2018-04-26 16:35:31 +0000

    re-tested set methods under new contract

--- tests/HashSet.test.js
+++ tests/HashSet.test.js
@@ -16,29 +16,31 @@ describe("HashSet", function() {
     Set2 = null;
   });
 
-  it("add should add element to set", function() {
-    Set.add("a");
-    Set.add("b");
-    Set.add("c");
-    expect(Set.has("a")).to.be.equal(true);
-    expect(Set.has("b")).to.be.equal(true);
-    expect(Set.has("c")).to.be.equal(true);
-    expect(Set.cardinality()).to.be.equal(3);
-  });
-
-  it("intersect should update 'this' set to contain intersection of the two sets", function() {
+  it("intersect should return intersection when this set is larger than that set", function() {
     Set.add("a");
     Set.add("b");
     Set.add("c");
     Set2.add("b");
     Set2.add("c");
-    Set.intersect(Set2);
-    expect(Set.has("a")).to.be.equal(false);
-    expect(Set.has("b")).to.be.equal(true);
-    expect(Set.has("c")).to.be.equal(true);
+    expect(Set.intersect(Set2)).to.have.members(["b", "c"]);
+  });
+  
+  it("intersect should return intersection when this set is same size of that set", function() {
+    Set.add("a");
+    Set.add("b");
+    Set2.add("b");
+    Set2.add("c");
+    expect(Set.intersect(Set2)).to.have.members(["b"]);
+  });
+  
+  it("intersect should return intersection when this set is smaller than that set", function() {
+    Set.add("a");
+    Set.add("b");
+    Set2.add("b").add("c").add("d").add("e");
+    expect(Set.intersect(Set2)).to.have.members(["b"]);
   });
 
-  it("intersect should not update 'this' set when two sets have no intersection", function() {
+  it("intersect should return empty array when two sets have no intersection", function() {
     Set.add("a");
     Set.add("b");
     Set.add("c");
@@ -46,83 +48,83 @@ describe("HashSet", function() {
     Set2.add("f");
     Set2.add("g");
     Set2.add("r");
-    Set.intersect(Set2);
-    expect(Set.has("a")).to.be.equal(false);
-    expect(Set.has("b")).to.be.equal(false);
-    expect(Set.has("c")).to.be.equal(false);
-    expect(Set.cardinality()).to.be.equal(0);
-    expect(Set2.cardinality()).to.be.equal(4);
+    expect(Set.intersect(Set2)).to.have.members([]);
   });
 
-  it("intersect should update 'this' set with intersection when other set is empty", function() {
+  it("intersect should return empty array when other set is empty", function() {
     Set.add("a");
-    Set.intersect(Set2);
-    expect(Set.cardinality()).to.be.equal(0);
-    expect(Set2.cardinality()).to.be.equal(0);
+    expect(Set.intersect(Set2)).to.have.members([]);
   });
 
 
-  it("intersect should update 'this' set with intersection when 'this' is empty", function() {
+  it("intersect should return empty array when 'this' is empty", function() {
     Set2.add("b");
     Set.intersect(Set2);
-    expect(Set.cardinality()).to.be.equal(0);
-    expect(Set2.cardinality()).to.be.equal(1);
+    expect(Set.intersect(Set2)).to.have.members([]);
   });
 
-  it("intersect should update 'this' set with intersection when both are empty", function() {
+  it("intersect should return empty array when both are empty", function() {
     Set.intersect(Set2);
-    expect(Set.cardinality()).to.be.equal(0);
-    expect(Set2.cardinality()).to.be.equal(0);
+    expect(Set.intersect(Set2)).to.have.members([]);
   });
 
-  it("diff should update 'this' set with difference when no difference", function() {
+  it("diff should return this set when no difference", function() {
     Set.add("a");
     Set.add("b");
     Set.add("c");
     Set2.add("d");
     Set2.add("e");
     Set2.add("f");
-    Set.diff(Set2);
-    expect(Set.has("a")).to.be.equal(true);
-    expect(Set.has("b")).to.be.equal(true);
-    expect(Set.has("c")).to.be.equal(true);
-    expect(Set.cardinality()).to.be.equal(3);
-    expect(Set2.cardinality()).to.be.equal(3);
+    expect(Set.diff(Set2)).to.have.members(["a", "b", "c"]);
   });
 
-  it("diff should update 'this' set with difference when difference", function() {
+  it("diff should return difference when difference", function() {
     Set.add("a");
     Set.add("b");
     Set.add("c");
     Set2.add("c");
-    Set.diff(Set2);
-    expect(Set.has("a")).to.be.equal(true);
-    expect(Set.has("b")).to.be.equal(true);
-    expect(Set.has("c")).to.be.equal(false);
-    expect(Set.cardinality()).to.be.equal(2);
-    expect(Set2.cardinality()).to.be.equal(1);
+    expect(Set.diff(Set2)).to.have.members(["a", "b"]);
   });
-
-  it("diff should update this with difference when 'this' set is empty", function() {
-    Set2.add("f");
-    Set.diff(Set2);
-    Set.intersect(Set2);
-    expect(Set.cardinality()).to.be.equal(0);
-    expect(Set2.cardinality()).to.be.equal(1);
-  });
-
-  it("diff should update 'this' set with difference when other set is empty", function() {
+  
+  it("diff should return empty array for equal sets", function() {
     Set.add("a");
     Set.add("b");
     Set.add("c");
-    Set.diff(Set2);
-    expect(Set.cardinality()).to.be.equal(3);
-    expect(Set2.cardinality()).to.be.equal(0);
+    Set2.add("c").add("b").add("a");
+    expect(Set.diff(Set2)).to.have.members([]);
   });
 
-  it("diff should update 'this' set with difference when both are empty", function() {
-    Set.diff(Set2);
-    expect(Set.cardinality()).to.be.equal(0);
-    expect(Set2.cardinality()).to.be.equal(0);
+  it("diff should return empty array 'this' set is empty", function() {
+    Set2.add("f");
+    expect(Set.diff(Set2)).to.have.members([]);
+  });
+
+  it("diff should return difference when other set is empty", function() {
+    Set.add("a");
+    Set.add("b");
+    Set.add("c");
+    expect(Set.diff(Set2)).to.have.members(["a", "b", "c"]);
+  });
+
+  it("diff should return empty array when both are empty", function() {
+    expect(Set.diff(Set2)).to.have.members([]);
+  });
+  
+  
+  it("union should return empty array when both are empty", function() {
+    expect(Set.union(Set2)).to.have.members([]);
+   
+  });
+  
+  it("union should not include duplicates in union", function() {
+    Set.add(9).add(10);
+    Set2.add(9).add(11).add(10);
+    expect(Set.union(Set2)).to.have.members([9, 10, 11]);
+  });
+  
+  it("union should return union when both sets are totally different", function() {
+    Set.add(1).add(2).add(3);
+    Set2.add(9).add(13);
+    expect(Set.union(Set2)).to.have.members([1, 2, 3, 9, 13]);
   });
 });
