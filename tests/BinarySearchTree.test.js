1308e89f0b5b826bdf90be862ee05735b7d799c2
Eddie Cornelious <eulerissmart@gmail.com>
2018-09-06 03:33:22 +0000

    test: added more tests

--- tests/BinarySearchTree.test.js
+++ tests/BinarySearchTree.test.js
@@ -10,6 +10,15 @@ describe('Binary Search Tree', function() {
   afterEach(function() {
     bst, expected, (actual = null);
   });
+  
+  it('clear should clear the tree', function() {
+    bst.put('a', 1);
+    bst.put('c');
+    bst.clear();
+    expect(bst.size()).to.be.equal(0);
+    expect(bst.keys()).to.have.ordered.members([]);
+    
+  });
 
   it('put should put into empty tree', function() {
     expected = ['a'];
@@ -35,6 +44,12 @@ describe('Binary Search Tree', function() {
     expect(bst.contains(0)).to.be.equal(true);
     expect(bst.contains(true)).to.be.equal(true);
     expect(bst.contains(1)).to.be.equal(false);
+  });
+  
+   it('put inserts null key and value when both are undefined', function() {
+    bst.put();
+    expect(bst.size()).to.be.equal(1);
+    expect(bst.contains(null)).to.be.equal(true);
   });
 
   it('put should put into non empty tree right', function() {
