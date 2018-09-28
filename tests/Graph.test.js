ca6af5e6335377cf707414a02039ef0b4a29f16b
Eddie Cornelious <eulerissmart@gmail.com>
2018-09-28 03:34:45 +0000

    test: added more tests

--- tests/Graph.test.js
+++ tests/Graph.test.js
@@ -11,15 +11,43 @@ describe('Graph', function() {
   afterEach(function() {
     graph, expected, (actual = null);
   });
+  
+  it('addVertex should not add the same vertex twice', function() {
+    graph.addVertex(0);
+    graph.addVertex(0);
+    expected = [0];
+    actual = graph.getVerticies();
+    expect(actual).to.have.ordered.members(expected);
+  });
+  
+  it('addEdge should only add edge if both vertieices exist', function() {
+    graph.addVertex(0);
+    graph.addVertex(0);
+    graph.addEdge("p", "q");
+    graph.addEdge(0, "q");
+    expected = [];
+    actual = graph.getNeighbors(0);
+    expect(actual).to.have.ordered.members(expected);
+  });
+  
+  it('addEdge should not add duplicate edges', function() {
+    graph.addVertex(0);
+    graph.addVertex(1);
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 1);
+    expected = 1;
+    actual = graph.getNeighbors(0).length;
+    expect(1).to.be.equal(expected);
+  });
 
   it('BFS should return bfs with connected graph', function() {
     graph.addVertex(0);
     graph.addVertex(1);
     graph.addVertex(2);
     graph.addEdge(0, 1);
-    graph.addEdge(2, 1);
+    graph.addEdge(1, 2);
     graph.addEdge(2, 0);
-    expected = [2, 1, 0];
+    expected = [2, 0, 1];
     actual = graph.BFS(2);
     for (let i = 0; i < actual.length; i += 1) {
       expect(actual[i]).to.be.equal(expected[i]);
@@ -34,7 +62,7 @@ describe('Graph', function() {
     graph.addVertex(9);
     graph.addEdge(0, 1);
     graph.addEdge(1, 2);
-    expected = [2, 1, 0];
+    expected = [2];
     actual = graph.BFS(2);
     for (let i = 0; i < actual.length; i += 1) {
       expect(actual[i]).to.be.equal(expected[i]);
@@ -58,12 +86,10 @@ describe('Graph', function() {
     graph.addVertex(0);
     graph.addVertex(1);
     graph.addVertex(2);
-    graph.addVertex(6);
-    graph.addEdge(0, 1);
+    graph.addEdge(0, 2);
     graph.addEdge(2, 1);
-    graph.addEdge(2, 0);
-    graph.addEdge(2, 6);
-    expected = [0, 2, 6, 1];
+    graph.addEdge(1, 0);
+    expected = [0, 2, 1];
     actual = graph.DFS(0);
     for (let i = 0; i < actual.length; i += 1) {
       expect(actual[i]).to.be.equal(expected[i]);
@@ -75,11 +101,10 @@ describe('Graph', function() {
     graph.addVertex(0);
     graph.addVertex(1);
     graph.addVertex(2);
-    graph.addVertex(6);
     graph.addEdge(0, 1);
     graph.addEdge(2, 1);
     graph.addEdge(2, 0);
-    expected = [0, 2, 1];
+    expected = [0, 1, 2];
     actual = graph.DFS(0);
     for (let i = 0; i < actual.length; i += 1) {
       expect(actual[i]).to.be.equal(expected[i]);
